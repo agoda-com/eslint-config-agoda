9c6bc17ccfbed24a5d9114ef98f9afa49cd073f4
Thatchapon Unprasert <thatchapon.unprasert@agoda.com>
2024-11-11 15:03:16 +0700

    Fix

--- ci/modify-package-version.sh
+++ ci/modify-package-version.sh
@@ -10,12 +10,6 @@ then
   exit 1
 fi
 
-FILES=$(find packages -name 'package.json')
+FILE='package.json'
 
-for FILE in $FILES
-do
-  if $(jq -e '.private!=true' $FILE > /dev/null)
-  then
-    jq --indent 4 --arg NEW_VERSION "$NEW_VERSION" '.version=$NEW_VERSION' $FILE > temp.json && mv temp.json $FILE
-  fi
-done
+jq --indent 4 --arg NEW_VERSION "$NEW_VERSION" '.version=$NEW_VERSION' $FILE > temp.json && mv temp.json $FILE
